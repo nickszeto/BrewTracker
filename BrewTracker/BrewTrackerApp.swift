f23507868fba740e9c721402c47e1d6a3bad71f1
Nicholas Szeto <szetonick@gmail.com>
2021-12-17 10:49:38 +0800

    Initial Commit

---
+++ BrewTracker/BrewTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewTrackerApp.swift
+//  BrewTracker
+//
+//  Created by Nicholas Szeto on 17/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BrewTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrewTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrewTracker
+//
+//  Created by Nicholas Szeto on 17/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
